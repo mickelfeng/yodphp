a2353a00a0c084541158e70c6ad64c2666b72c87
zmrnet <zmrnet@qq.com>
2013-12-06 12:26:19 +0800

    @ demo

--- demo/demo.php
+++ demo/demo.php
@@ -5,12 +5,35 @@ date_default_timezone_set('Asia/Shanghai');
 
 define('YOD_PATHVAR', 'r');
 define('YOD_RUNPATH', dirname(__FILE__) . '/app');
+
+// config
+$config = array(
+	'db_dsn' => array(
+		'type' => 'pdo',
+		'dsn'  => 'mysql:host=localhost;port=3306;dbname=test',
+		'host' => 'localhost',
+		'user' => 'root',
+		'pass' => '',
+		'dbname' => 'test',
+		'prefix' => 'yod_',
+		'slaves' => array(
+
+		),
+	),
+);
+
 class_exists('Yod_Application', false) or require YOD_RUNPATH . '/../../yodphp/yodphp.php';
 
 class DemoController extends Yod_Controller
 {
 	public function indexAction()
 	{
+		$model = Yod_Model::getInstance();
+		$demo = $this->model('demo');
+
+		echo '<pre>';
+		print_r($model);
+
 		$this->display();
 	}
 
@@ -30,4 +53,9 @@ class DemoController extends Yod_Controller
 		echo '<pre>';
 		print_r(Yod_Application::app());
 	}
+}
+
+class DemoModel extends Yod_Model
+{
+
 }
