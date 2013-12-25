4171de6b463b5d8aad0a2f28f8ece803b01f9287
zmrnet <zmrnet@qq.com>
2013-12-25 20:43:11 +0800

    @ test

--- test/index.php
+++ test/index.php
@@ -8,6 +8,6 @@ defined('YOD_RUNPATH') or define('YOD_RUNPATH', dirname(__FILE__) . '/testapp');
 defined('YOD_EXTPATH') or define('YOD_EXTPATH', dirname(__FILE__) . '/../yodphp');
 defined('YOD_LOGFILE') or define('YOD_LOGFILE', dirname(__FILE__) . '/debugs/' . date('YmdHis') . '.log');
 
-defined('APP_WEBROOT') or define('APP_WEBROOT', '/yodphp/test/index.php');
+defined('APP_WEBROOT') or define('APP_WEBROOT', '/index.php');
 
 class_exists('Yod_Application', false) or require dirname(__FILE__) . '/../yodphp/yodphp.php';
