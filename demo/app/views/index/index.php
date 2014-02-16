517c905f6d72b92caeb4e166de884af407583387
zmrnet <zmrnet@qq.com>
2014-02-16 18:00:16 +0800

    @ readme

--- demo/app/views/index/index.php
+++ demo/app/views/index/index.php
@@ -78,7 +78,6 @@ error_reporting(E_ALL);
 date_default_timezone_set('Asia/Shanghai');
 
 defined('YOD_RUNPATH') or define('YOD_RUNPATH', dirname(__FILE__) . '/app');
-class_exists('Yod_Application', false) or require YOD_RUNPATH . '/../yodphp/yodphp.php';
 
 "
 ); ?><pre>-------------------------------------------------------------------------</pre></p>
@@ -92,7 +91,6 @@ error_reporting(E_ALL);
 date_default_timezone_set('Asia/Shanghai');
 
 defined('YOD_RUNPATH') or define('YOD_RUNPATH', dirname(__FILE__) . '/app');
-class_exists('Yod_Application', false) or require YOD_RUNPATH . '/yodphp.php';
 
 class HelloController extends Yod_Controller
 {
